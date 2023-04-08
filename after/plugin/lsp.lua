cd09a1d4389533fb935d7f97ed3167c0e981cc63
Sungbae Jeong <almagest0328@gmail.com>
2023-04-08 15:17:33 +0900

    foo

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -27,7 +27,7 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	["<C-b>"] = cmp.mapping.scroll_docs(4),
 	["<C-f>"] = cmp.mapping.scroll_docs(-4),
-	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<CR>"] = cmp.mapping.confirm({ select = true }),
 	["<C-Space>"] = cmp.mapping.confirm({
 		behavior = cmp.ConfirmBehavior.Insert,
 		select = true,
