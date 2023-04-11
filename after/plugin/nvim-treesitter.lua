a87479315d3fe2e5a83f79661a08bd1e38606315
Sungbae Jeong <almagest0328@gmail.com>
2023-04-11 17:52:08 +0900

    fixing error

--- after/plugin/nvim-treesitter.lua
+++ after/plugin/nvim-treesitter.lua
@@ -1,12 +1,32 @@
 local utils = require("almagest.utils")
 
 utils.cfgplugin("nvim-treesitter.configs", {
-    ensure_installed = {
-        "bash", "c", "cmake", "cpp", "css", "fish", "glsl", "go", "haskell",
-        "help", "html", "java", "javascript", "kotlin", "lua", "meson", "rust",
-        "scss", "toml", "typescript", "vim", "vue", "wgsl", "zig"
-    },
-    sync_install = false,
-    auto_install = true,
-    highlight = {enable = true, additional_vim_regex_highlighting = false}
+	ensure_installed = {
+		"bash",
+		"c",
+		"cmake",
+		"cpp",
+		"css",
+		"fish",
+		"glsl",
+		"go",
+		"haskell",
+		"html",
+		"java",
+		"javascript",
+		"kotlin",
+		"lua",
+		"meson",
+		"rust",
+		"scss",
+		"toml",
+		"typescript",
+		"vim",
+		"vue",
+		"wgsl",
+		"zig",
+	},
+	sync_install = false,
+	auto_install = true,
+	highlight = { enable = true, additional_vim_regex_highlighting = false },
 })
