10d40993808a461b2be9096ace044274e4a65fb6
Sungbae Jeong <almagest0328@gmail.com>
2023-10-16 15:39:16 +0900

    fix tab issue in luasnip mode

--- after/plugin/snippets.lua
+++ after/plugin/snippets.lua
@@ -7,15 +7,21 @@ utils.cfgplugin("luasnip", function(ls)
 	local fmt = require("luasnip.extras.fmt").fmt
 
 	-- Keybindings
-	vim.keymap.set({ "i" }, "<C-]>", function()
-		ls.expand()
-	end, { silent = true })
-	vim.keymap.set({ "i", "s" }, "<C-]>", function()
-		ls.jump(1)
-	end, { silent = true })
-	vim.keymap.set({ "i", "s" }, "<S-C-]>", function()
-		ls.jump(-1)
-	end, { silent = true })
+	-- NOTE: Stolen from https://github.com/L3MON4D3/LuaSnip
+	vim.cmd([[
+    " press <Tab> to expand or jump in a snippet. These can also be mapped separately
+    " via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
+    imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+    " -1 for jumping backwards.
+    inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
+
+    snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
+    snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+
+    " For changing choices in choiceNodes (not strictly necessary for a basic setup).
+    imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+    smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+    ]])
 
 	--[[
 	vim.keymap.set({ "i", "s" }, "<C-E>", function()
