fe3fd52d74bba690707e3cc80f5e2fd7311a175a
Sungbae Jeong <almagest0328@gmail.com>
2024-07-01 18:53:14 +0900

    foo

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -209,7 +209,7 @@ local lsp_configurations = {
 			filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
 		},
 	},
-	--[=[{
+	{
 		name = "rust_analyzer",
 		config = {
 			settings = {
@@ -223,7 +223,7 @@ local lsp_configurations = {
 				},
 			},
 		},
-	},--]=]
+	},
 }
 
 for _, lsp in ipairs(lsp_configurations) do
