329470a4363043984ccf59a5a93c86420a419ec8
Sungbae Jeong <almagest0328@gmail.com>
2025-08-20 14:28:51 +0900

    foo

---
+++ after/queries/highlights.scm
@@ -0,0 +1,44 @@
+; Tree-sitter highlighting queries for the vesti language
+
+;; Keywords
+((KEYWORD_docclass) @keyword)
+((KEYWORD_importpkg) @keyword)
+((KEYWORD_importmod) @keyword)
+((KEYWORD_copyfile) @keyword)
+((KEYWORD_importves) @keyword)
+((KEYWORD_useltx3) @keyword)
+((KEYWORD_getfp) @keyword)
+((KEYWORD_startdoc) @keyword)
+((KEYWORD_useenv) @keyword)
+((KEYWORD_begenv) @keyword)
+((KEYWORD_endenv) @keyword)
+((KEYWORD_makeatletter) @keyword)
+((KEYWORD_makeatother) @keyword)
+((KEYWORD_ltx3on) @keyword)
+((KEYWORD_ltx3off) @keyword)
+((KEYWORD_nonstopmode) @keyword)
+((KEYWORD_mathmode) @keyword)
+((KEYWORD_compty) @keyword)
+((KEYWORD_pycode) @keyword)
+(pycode_block (pycode_start) @keyword)
+(pycode_block (pycode_end)   @keyword)
+(pycode_block (pycode_imports) @constant)
+
+;; Identifiers
+((class_pkg_name) @type)
+((env_name) @type)
+((latex_function_name) @function)
+
+((inline_math) @string)
+((display_math) @string)
+
+((singleline_raw_latex) @type)
+((multiline_raw_latex) @type)
+
+;; Options and environment arguments
+((options) @constant)
+((mandantory_arg) @constant)
+((optional_arg) @constant)
+
+;; Comments
+((comment) @comment)
---
+++ after/queries/injections.scm
@@ -0,0 +1,6 @@
+; Combine all line contents into a single Python virtual buffer
+(pycode_block
+  (pycode_line
+    (pycode_line_content) @injection.content)
+  (#set! injection.language "python")
+  (#set! injection.combined "true"))
