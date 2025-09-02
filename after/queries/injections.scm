20fa6e9458861aa656180903ec2c155625fb5ce8
Sungbae Jeong <almagest0328@gmail.com>
2025-09-02 11:21:06 +0900

    fooo

--- after/queries/injections.scm
+++
@@ -1,6 +0,0 @@
-; Combine all line contents into a single Python virtual buffer
-(pycode_block
-  (pycode_line
-    (pycode_line_content) @injection.content)
-  (#set! injection.language "python")
-  (#set! injection.combined "true"))
